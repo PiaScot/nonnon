e435ff63abe0f1d987f55501e54b25069e13ca16
PiaScot <crcrunchypp0215@gmail.com>
2025-10-17 10:21:25 +0900

    modify inline css injection code

--- src/services/html-processor.ts
+++ src/services/html-processor.ts
@@ -89,7 +89,6 @@ function injectResetCSS($: cheerio.CheerioAPI): void {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        line-height: 0px;
       }
 
       /* ブラウザデフォルトスタイル除去 */
